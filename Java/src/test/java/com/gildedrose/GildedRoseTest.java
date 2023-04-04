846ebb25fe8ffce42bf9b672b2af0aac13f4faf3
obarnerias <olivier.barnerias@lafabriquedigitowl.com>
2023-04-04 14:48:32 -0400

    - Add an automated reset to the tested app for each unit test - Add sulfuras quality treatment unit test - Change Default "foo" test to the common item nominal case

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,44 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+    private Item[] items;
+    private GildedRose app;
+    @BeforeEach
+    void initItems(){
+        items = new Item[] {
+            new Item("Elixir of the Mongoose", 5, 7),
+            new Item("Sulfuras, Hand of Ragnaros", 1, 80)
+        };
+        app = new GildedRose(items);
+    }
+
+    @AfterEach
+    void clearItems(){
+        items = null;
+        app = null;
+    }
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
+    void givenCommonItemWithNotPassedSellDate_whenComeEndOfTheDay_thenQualityIsAltered() {
+        int expectedQuality = app.items[0].quality - 1;
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(expectedQuality, app.items[0].quality);
+    }
+
+    @Test
+    void givenSulfuras_whenComeEndOfTheDay_thenQualityIsNotAltered(){
+        int sulfurasExpectedQuality = 80;
+
+        app.updateQuality();
+
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[1].name);
+        assertEquals(sulfurasExpectedQuality, app.items[1].quality);
     }
 
 }
